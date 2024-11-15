7a359dd990aa0c0d54dea1175020988e6e7fb133
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-11-15 16:53:50 -0300

    feat: adiciona cores globais para o projeto

---
+++ src/constants/theme.js
@@ -0,0 +1,19 @@
+const colors = {
+    red: '#ED5359',
+    darkGray: '#5D6275',
+    mediumGray: '#A6ABBC',
+    lightGray: '#E5ECFC',
+    white: '#F4F7FE',
+    lightGreen: '#38C682'
+};
+
+const fontSize = {
+    xsm: 11,
+    sm: 14,
+    md: 16,
+    lg: 22,
+    xl: 30,
+    xxl: 64,
+};
+
+export { colors, fontSize };
