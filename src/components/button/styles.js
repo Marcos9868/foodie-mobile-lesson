0d087d5c5426800b65c52316b2f312efaa08003a
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-11-15 16:55:05 -0300

    feat: adiciona estilos

---
+++ src/components/button/styles.js
@@ -0,0 +1,15 @@
+import { colors, fontSize } from '../../constants/theme.js';
+
+export const styles = {
+    btn: {
+        width: '100%',
+        height: 50,
+        backgroundColor: colors.red
+    },
+    textos: {
+        fontSize: fontSize.md,
+        color: colors.white,
+        padding: 12,
+        textAlign: 'center'
+    }
+};
