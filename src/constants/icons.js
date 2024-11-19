f9b8b0496315c783a7f4792b758e36eb99323dd3
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-11-18 22:34:03 -0300

    feat: add logo on icons

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -1,7 +1,9 @@
 import back from '../assets/back.png';
 import remove from '../assets/delete.png';
+import logo from '../assets/logo.png';
 
 export default {
     back, 
-    remove
+    remove,
+    logo
 };
